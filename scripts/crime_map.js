e20302ec48f1c1c19097e5ff32da16ed9b4ef8fa
Bennett <liambennett99@yahoo.co.uk>
2022-09-25 22:31:28 +0100

    Changed site css Added mouse movement check for api calls Frame rate cap removed Javascript page renamed

--- scripts/crime_map.js
+++ scripts/crime_map.js
@@ -1,27 +1,39 @@
 function preload() {
   img = loadImage("scripts/assets/area_map.jpg");
 }
+var canvasWidth = 1773;
+var canvasHeight = 791;
 function setup() {
-  createCanvas(1773, 791);
-  pg = createGraphics(1773, 791);
-  frameRate(10);
+  createCanvas(canvasWidth, canvasHeight);
+  pg = createGraphics(canvasWidth, canvasHeight);
+  setInterval(getCrimeData, 250);
 }
+
 function draw() {
   pg.clear();
-  pg.ellipse(mouseX,mouseY,120,120);
+  pg.ellipse(mouseX,mouseY,60,60);
   image(img, 0, 0);
   image(pg, 0, 0);
-	var mapmousex = map(mouseX,0,1773,-1.943066,-1.338474);
-	var mapmousey = map(mouseY,0,791,53.921540,53.763338);
+
+}
+
+var lastUrl;
+const getCrimeData = () =>{
+  var mapmousex = map(mouseX,0,canvasWidth,-1.943066,-1.338474);
+	var mapmousey = map(mouseY,0,canvasHeight,53.921540,53.763338);
   mapmousex = nf(mapmousex,1,6);
   mapmousey = nf(mapmousey,2,6);
-	var url = "https://data.police.uk/api/crimes-street/vehicle-crime?lat="+mapmousey+"&lng="+mapmousex+"&date=2018-08";
-	loadJSON(url,gotCrimeData);
-	function gotCrimeData(data) {
-    console.log(data.length);
-    crimenumb = data.length;
-    red = map(crimenumb,0,100,0,255);
-    green = map(crimenumb,0,100,255,0);
-    pg.fill(red,green,0);
+  var url = "https://data.police.uk/api/crimes-street/vehicle-crime?lat="+mapmousey+"&lng="+mapmousex+"&date=2022-01";
+  if(lastUrl!=url){
+	//loadJSON(url,gotCrimeData);
+  httpDo(url, "GET", "json", false, gotCrimeData)
+  lastUrl=url;
   }
+}
+
+const gotCrimeData = (data) => {
+  crimenumb = data.length;
+  red = map(crimenumb,0,100,0,255);
+  green = map(crimenumb,0,100,255,0);
+  pg.fill(red,green,0,127);
 }
